07c3e59b6cff14356a418a467869c1b7b71d36d1
Martin010 <martin.serret47@gmail.com>
2025-05-05 20:52:42 +0200

    Literal types

--- 02. TypeScript Basics & Basic Types/Code/choices.ts
+++ 02. TypeScript Basics & Basic Types/Code/choices.ts
@@ -1,17 +1,32 @@
-enum Role {   // enum is a feature offered by Typescript. Enum is a type that only allows certain kinds of options. You create a kind of custom type that you can use
+// ENUM (some developer try to avoid them)
+
+enum RoleEnum {   // enum is a feature offered by Typescript. Enum is a type that only allows certain kinds of options. You create a kind of custom type that you can use
   Admin,
   Editor,
   Guest,
 }; 
 
-let userRole: Role = Role.Admin; // or userRole: Role = 0 (or 1 and 2) but less explicit
+let userRoleEnum: RoleEnum = Role.Admin; // or userRole: Role = 0 (or 1 and 2) but less explicit
 
 // ...
 
 userRole = Role.Guest;
 
-enum RoleStrOption {   // You can choose any values you want 
+enum RoleEnumStrOption {   // You can choose any values you want 
   Admin = "Admin",
   Editor = "Editor",
   Guest = "Guest",
 };
+
+
+// LITERAL TYPES (MORE POPULAR)
+
+let userRoleLiteral: "admin" | "editor" | "guest" = "admin"; // After colon and before equal, no matter if you're using a variable or a parameter, this this is not a value but a TYPE 
+// (here there are 3 types possible for the variable userRoleLiteral)
+
+userRoleLiteral = "editor"
+//! userRoleLiteral = "other" // Error because "other" is not an option for userRoleLiteral
+
+let possibleResultsLiteral: [1 | -1, number];
+possibleResultsLiteral = [-1, 10]
+//! possibleResultsLiteral = [5, 10] // Error because 5 should be -1 or 1
