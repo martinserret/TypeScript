9faede17dbc5fd6f329825c25a5938411b7a11a5
Martin010 <martin.serret47@gmail.com>
2025-05-05 11:26:47 +0200

    Array types

---
+++ 02. TypeScript Basics & Basic Types/Code/arrays-objects.ts
@@ -0,0 +1,9 @@
+let hobbies = ["Sports", "Cookies"]; // Typescript understand that hobbies is a variable that stores an array full of string
+let notes: (number | string)[];
+
+hobbies.push("Yoga");
+//! hobbies.push(10); // not a string element
+
+notes = [12, "B", 15, 9, "A"];
+notes = ["C", "B", "B", "A"];
+notes = [11, 16, 12];
