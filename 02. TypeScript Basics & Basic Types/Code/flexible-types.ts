b8b0f3f38b4340d4495afc357d6e8442ccb021c6
Martin010 <martin.serret47@gmail.com>
2025-05-05 10:43:40 +0200

    Union types

--- 02. TypeScript Basics & Basic Types/Code/flexible-types.ts
+++ 02. TypeScript Basics & Basic Types/Code/flexible-types.ts
@@ -1,4 +1,5 @@
 let age: any = 36; // "any" type accepts any kind of value
+let size: string | number = 198 // called union type : size is a string OR a NUMBER (you can add more | if you want others types)
 
 // ...
 
@@ -6,3 +7,7 @@ age = "37";
 age = false;
 age = {};
 age = []
+
+size = "1m 98";
+size = 199
+//! size = false; // error because size is only string or number
