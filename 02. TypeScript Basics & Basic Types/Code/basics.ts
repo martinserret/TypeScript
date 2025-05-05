fbac75622a7ba6d197f9dffe61d9e4e4599854a6
Martin010 <martin.serret47@gmail.com>
2025-05-05 09:45:47 +0200

    Type inference vs type assignment

--- 02. TypeScript Basics & Basic Types/Code/basics.ts
+++ 02. TypeScript Basics & Basic Types/Code/basics.ts
@@ -1,6 +1,8 @@
-let userName: string; // TypeScript specific features
+let userName: string; // TypeScript specific features (type assignment or annotation)
+let userAge = 38; // TypeScript no explicit assignment (type inference)
 
-userName = 5
+userName = 40
+userAge = "Dwight"
 
 // Warning : value types are not TypeScript specific. Using typeof avec une variable, le type est de la variable est retourné
 // The idea of enforcing types, setting types explicitly and getting type related errors at the point of time are TypeScript specific
