ee2466a06ffd8ce150b5eaf9e4f01a59e2948f86
Martin010 <martin.serret47@gmail.com>
2025-05-05 09:38:58 +0200

    JavaScript has types too

--- 02. TypeScript Basics & Basic Types/Code/basics.ts
+++ 02. TypeScript Basics & Basic Types/Code/basics.ts
@@ -1,3 +1,6 @@
-let userName: string;
+let userName: string; // TypeScript specific features
 
 userName = 5
+
+// Warning : value types are not TypeScript specific. Using typeof avec une variable, le type est de la variable est retourné
+// The idea of enforcing types, setting types explicitly and getting type related errors at the point of time are TypeScript specific
---
+++ 02. TypeScript Basics & Basic Types/Code/vanilla-js-types.js
@@ -0,0 +1,3 @@
+let userName = "Max"
+
+console.log(typeof userName); //value types are not TypeScript specific. Using typeof avec une variable, le type est de la variable est retourné
