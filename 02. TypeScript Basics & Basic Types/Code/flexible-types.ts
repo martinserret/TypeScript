62cb83b51c7979aa577b3c90f66eaeb32937f2ab
Martin010 <martin.serret47@gmail.com>
2025-05-05 10:40:03 +0200

    The any type

---
+++ 02. TypeScript Basics & Basic Types/Code/flexible-types.ts
@@ -0,0 +1,8 @@
+let age: any = 36; // "any" type accepts any kind of value
+
+// ...
+
+age = "37";
+age = false;
+age = {};
+age = []
--- 02. TypeScript Basics & Basic Types/Code/vanilla-js-types.js
+++ 02. TypeScript Basics & Basic Types/Code/vanilla-js-types.js
@@ -1,3 +1,3 @@
-let userName = "Max"
+let userNameJS = "Max"
 
 console.log(typeof userName); //value types are not TypeScript specific. Using typeof avec une variable, le type est de la variable est retourné
