e4c6b6bf139afcc38234118e4867871628fc7a57
Martin010 <martin.serret47@gmail.com>
2025-05-07 10:56:36 +0200

    Installing type packages

--- 03. The TypeScript Compiler (and its Configuration)/code/dist/app.js
+++ 03. The TypeScript Compiler (and its Configuration)/code/dist/app.js
@@ -1,26 +1,24 @@
+"use strict";
 //*****************************************************************************************************************************************
 //* To create a Typescript configuration file (in terminal) : tsc --inits *
 //*****************************************************************************************************************************************
-
+Object.defineProperty(exports, "__esModule", { value: true });
 //*****************************************************************************************************************************************
 //* Language and Environment settings
 //* - "target" : very important setting that tell to Typescript in which version to compile the code in Javascript
 //* - "lib" : allow to include some standard type librairies
 //* - "jsx" : control how the JSX code should be treated when Typescript code contains JSX 
 //*****************************************************************************************************************************************
-
 //*****************************************************************************************************************************************
 //* Modules
 //* - "module" : allow how imports and exports work (NodeNext is a good choice for Node.js projects)
 //* - "rootDir" : control which folder wil contain the source files
 //*****************************************************************************************************************************************
-
 //*****************************************************************************************************************************************
 //* JavaScript Support
 //* - "allowJS" : allow combinaison of Javascript and Typescript in the project
 //* - "checkJS" : basic type checking in JS files
 //*****************************************************************************************************************************************
-
 //*****************************************************************************************************************************************
 //* Emit
 //* - "sourceMap" : when Typescript compiles the code, it also generates source maps which can help with debugging the running application
@@ -28,7 +26,6 @@
 //* - "outDir" : allow to specify a directory in which the compiled files will be stored
 //* - "noEmitOnError" : ensure that no new files are created and output if there is an error during compilation
 //*****************************************************************************************************************************************
-
 //*****************************************************************************************************************************************
 //* Type Checking : control how strict Typescript is and about which kind of potential errors it will complain
 //* - "strict" : turn on most of these settings (which does not allow a lot of things but is a good practice)
@@ -36,26 +33,9 @@
 //* "noUnusedParameters": help to detect unused function parameters
 //* "noFallthroughCasesInSwitch": help to detect switch cases without break or return
 //*****************************************************************************************************************************************
-
-
-//*****************************************************************************************************************************************
-//* Some packages like node, React etc. are not natively supported by Typescript (the related types are not built into Typescript)
-//* To correct that, you have to install an extra type packages in the project called @types/yourPackage (npm install @types/node,
-//* npm install @types/react, npm install @types/lodash)
-
-//* https://github.com/DefinitelyTyped/DefinitelyTyped
-//*****************************************************************************************************************************************
-
-import fs from 'node:fs'
-
-//fs.readFileSync()
-
-let userName: string;
-
+let userName;
 userName = "Dwight";
-
 console.log(userName);
-
-function add(a: any, b: any) {
-  return a + b;
+function add(a, b) {
+    return a + b;
 }
