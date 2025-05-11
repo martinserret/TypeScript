bb0bffa407ab927f011bac5a85098396ed438f9f
Martin010 <martin.serret47@gmail.com>
2025-05-11 15:46:19 +0200

    let & const

--- 05. Next-generation JavaScript & TypeScript/code/project/src/app.ts
+++ 05. Next-generation JavaScript & TypeScript/code/project/src/app.ts
@@ -1,1 +1,8 @@
-// Code goes here!
+// LET & CONST
+// ---------------------
+
+const userName = "Max"; // "const" can not be change
+let age = 30; // "let" can be change. Similar to "var". Problem with "var": available everywhere (if you create a "var" inside a "if" condition, this "var" is available outside the "if" even if you don't go in the condition and the "var" is not define)
+
+age = 29; 
+
