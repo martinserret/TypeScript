65d3f69374e53700906bf80dcba6a92786dc1ebd
Martin010 <martin.serret47@gmail.com>
2025-05-11 17:36:52 +0200

    Default function parameters

--- 05. Next-generation JavaScript & TypeScript/code/project/src/app.ts
+++ 05. Next-generation JavaScript & TypeScript/code/project/src/app.ts
@@ -10,6 +10,11 @@ age = 29;
 // ARROW FUNCTIONS
 // ----------------------
 
+// Advantages : 
+//  - shorter
+//  - one line expression : no need curly bracket and no return
+// - function with only one parameter : concise syntax
+
 const add = (a: number, b: number) => a + b;
 
 const button = document.querySelector('button');
@@ -19,7 +24,17 @@ if(button) {
 }
 
 
-// Advantages : 
-//  - shorter
-//  - one line expression : no need curly bracket and no return
-// - function with only one parameter : concise syntax
+
+
+// DEFAULT FUNCTION PARAMETERS
+// ---------------------------------
+
+// The default parameter(s) have to be last in the list of parameters.
+// When you call a function with arguments, theses arguments are provided to the parameters in the order the parameters are defined. So if the default parameters are at the beginning, theses parameters take the value of the firsts arguments and the last parameters don't have values.
+
+const subtract = (a: number, b: number = 1) => a + b;
+console.log(subtract(5));
+
+//! Exemple of error
+// const subtractProblem = (a: number = 5, b: number) => a + b;
+// console.log(subtractProblem(5));
