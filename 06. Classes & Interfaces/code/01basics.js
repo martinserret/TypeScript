c8f88da53cd8a449e9ee21e27056fa814cd8ddc5
Martin010 <martin.serret47@gmail.com>
2025-05-12 17:39:18 +0200

    Public, private, protected

--- 06. Classes & Interfaces/code/01basics.js
+++ 06. Classes & Interfaces/code/01basics.js
@@ -1,4 +1,6 @@
 "use strict";
+// FIRST CLASS
+// -----------------
 class User {
     name; //* Typescript specific (in JS the constructor create the properties automatically behind the scene)
     age; //* Typescript specific (in JS the constructor create the properties automatically behind the scene)
@@ -8,10 +10,14 @@ class User {
     }
 }
 ;
+const dwight = new User('Dwight', 40);
+console.log(dwight);
+// TYPESCRIPT SHORTCUT
+// ------------------------
 class ConciseUser {
     name;
     age;
-    constructor(name, age = 0) {
+    constructor(name, age) {
         this.name = name;
         this.age = age;
     }
@@ -27,7 +33,30 @@ class TypescriptUser {
     }
     ; //* The constructor is just a function, so you dan use all the Typescript features
 }
-const dwight = new User('Dwight', 40);
 const jim = new ConciseUser('Jim', 35);
 const unknown = new TypescriptUser();
 console.log(dwight, jim, unknown);
+// PUBLIC, PRIVATE & PROTECTED
+// -----------------------
+// public: you can use public to control how a property may be accessed on the object that is created based on the class
+// private: can't access property outside of the class
+// protected: works like private but also makes sure that this property may be accessed in classes that inherit from this class
+class PublicPrivateUser {
+    name;
+    age;
+    hobbies = []; // can be access everywhere. Public is default here if you write nothing
+    constructor(name, age) {
+        this.name = name;
+        this.age = age;
+    }
+    ;
+    greet() {
+        console.log(`My age is ${this.age}`);
+    }
+}
+const michael = new PublicPrivateUser("Michael", 45);
+console.log(michael.hobbies);
+michael.hobbies = ["Movies"];
+console.log(michael.hobbies);
+// michael.age = 42; //! example of error because age is private
+michael.greet();
--- 06. Classes & Interfaces/code/01basics.ts
+++ 06. Classes & Interfaces/code/01basics.ts
@@ -1,3 +1,6 @@
+// FIRST CLASS
+// -----------------
+
 class User {
   name: string; //* Typescript specific (in JS the constructor create the properties automatically behind the scene)
   age: number; //* Typescript specific (in JS the constructor create the properties automatically behind the scene)
@@ -8,16 +11,50 @@ class User {
   }
 };
 
+const dwight = new User('Dwight', 40);
+console.log(dwight);
+
+
+// TYPESCRIPT SHORTCUT
+// ------------------------
+
 class ConciseUser {
-  constructor(public name: string, public age: number = 0) {}; //* With Typescript, other way to create properties and assign the value automatically behind the scene : use "public" or "private" before the parameters
+  constructor(public name: string, public age: number) {}; //* With Typescript, other way to create properties and assign the value automatically behind the scene : use "public" or "private" before the parameters
 };
+
 
 class TypescriptUser {
   constructor(public name: string = "John", public age?: number) {}; //* The constructor is just a function, so you dan use all the Typescript features
 }
 
-const dwight = new User('Dwight', 40);
 const jim = new ConciseUser('Jim', 35);
 const unknown = new TypescriptUser();
 
 console.log(dwight, jim, unknown);
+
+
+// PUBLIC, PRIVATE & PROTECTED
+// -----------------------
+
+// public: you can use public to control how a property may be accessed on the object that is created based on the class
+// private: can't access property outside of the class
+// protected: works like private but also makes sure that this property may be accessed in classes that inherit from this class
+
+class PublicPrivateUser {
+  public hobbies: string[] = []; // can be access everywhere. Public is default here if you write nothing
+
+  constructor(public name: string, private age: number) {};
+
+  greet() {
+    console.log(`My age is ${this.age}`);
+  }
+}
+
+const michael = new PublicPrivateUser("Michael", 45);
+
+console.log(michael.hobbies);
+michael.hobbies = ["Movies"];
+console.log(michael.hobbies);
+
+// michael.age = 42; //! example of error because age is private
+michael.greet();
