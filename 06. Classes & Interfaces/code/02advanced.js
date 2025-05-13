84baeaf99b1313bbcb7a2bb2ea0554587961d1ca
Martin010 <martin.serret47@gmail.com>
2025-05-13 16:18:24 +0200

    Setters

--- 06. Classes & Interfaces/code/02advanced.js
+++ 06. Classes & Interfaces/code/02advanced.js
@@ -17,3 +17,35 @@ class UserGetter {
 }
 const dwightGetter = new UserGetter("Dwight", "Schrute");
 console.log(dwightGetter.fullName);
+// SETTERS
+// ---------------
+// setter: special kind of property or method create with the keyword "set". 
+// In a setter, you can change the value of a property and make checks or calculations before change it
+// You access to getter like a property even though it's define like a method.
+class UserSetter {
+    _firstName;
+    _lastName;
+    constructor(_firstName, _lastName) {
+        this._firstName = _firstName;
+        this._lastName = _lastName;
+    }
+    set firstName(name) {
+        if (name.trim() === '') {
+            throw new Error('Invalid name');
+        }
+        this._firstName = name;
+    }
+    set lastName(name) {
+        if (name.trim() === '') {
+            throw new Error('Invalid name');
+        }
+        this._lastName = name;
+    }
+    get fullName() {
+        return `${this._firstName} ${this._lastName}`;
+    }
+}
+const jimSetter = new UserSetter('Jim', 'Alpert"');
+jimSetter.firstName = 'James';
+jimSetter.lastName = 'Halpert';
+console.log(jimSetter.fullName);
--- 06. Classes & Interfaces/code/02advanced.ts
+++ 06. Classes & Interfaces/code/02advanced.ts
@@ -15,3 +15,44 @@ class UserGetter {
 
 const dwightGetter = new UserGetter("Dwight", "Schrute");
 console.log(dwightGetter.fullName);
+
+
+// SETTERS
+// ---------------
+
+// setter: special kind of property or method create with the keyword "set". 
+// In a setter, you can change the value of a property and make checks or calculations before change it
+// You access to getter like a property even though it's define like a method.
+
+class UserSetter {
+  constructor(private _firstName: string, private _lastName: string) {}
+
+  set firstName(name: string) {
+    if(name.trim() === '')
+    {
+      throw new Error('Invalid name');
+    }
+
+    this._firstName = name;
+  }
+
+  set lastName(name: string) {
+    if(name.trim() === '')
+    {
+      throw new Error('Invalid name');
+    }
+    
+    this._lastName = name;
+  }
+
+  get fullName() {
+    return `${this._firstName} ${this._lastName}`;
+  }
+}
+
+const jimSetter = new UserSetter('Jim', 'Alpert"');
+jimSetter.firstName = 'James';
+jimSetter.lastName = 'Halpert';
+
+console.log(jimSetter.fullName);
+
