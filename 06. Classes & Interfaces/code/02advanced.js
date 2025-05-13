c6229920fe92690b4974bfdadda5cf4c4fb27526
Martin010 <martin.serret47@gmail.com>
2025-05-13 17:31:52 +0200

    Static properties and methods

--- 06. Classes & Interfaces/code/02advanced.js
+++ 06. Classes & Interfaces/code/02advanced.js
@@ -49,3 +49,36 @@ const jimSetter = new UserSetter('Jim', 'Alpert"');
 jimSetter.firstName = 'James';
 jimSetter.lastName = 'Halpert';
 console.log(jimSetter.fullName);
+// STATIC PROPERTIES & METHODS
+// -------------------------------
+// static properties and method : you can access it on the class itself. Not on an object that's created based on a class, but on the class itself, before even creating an object.
+// This can especially be useful if you're building some kind of utility classes
+class UserStatic {
+    _firstName;
+    _lastName;
+    constructor(_firstName, _lastName) {
+        this._firstName = _firstName;
+        this._lastName = _lastName;
+    }
+    set firstName(name) {
+        if (name.trim() === '') {
+            throw new Error('Invalid name');
+        }
+        this._firstName = name;
+    }
+    set lastName(name) {
+        if (name.trim() === '') {
+            throw new Error('Invalid name');
+        }
+        this._lastName = name;
+    }
+    get fullName() {
+        return `${this._firstName} ${this._lastName}`;
+    }
+    static eid = "USER";
+    static greet() {
+        console.log("Hello !");
+    }
+}
+console.log(UserStatic.eid); // None object created here but I can call the properties "eid"
+UserStatic.greet(); // None object created here but I can call the method "greet"
