8593927e0b8609d8bd3ad807e6a1a745d06be53a
Martin010 <martin.serret47@gmail.com>
2025-05-14 11:10:46 +0200

    Interfaces as object types

--- 06. Classes & Interfaces/code/03interfaces.js
+++ 06. Classes & Interfaces/code/03interfaces.js
@@ -1,3 +1,16 @@
 "use strict";
 // INTERFACES
 // -----------------
+// INTERFACE AS OBJECT TYPES
+// -----------------------------------
+let user;
+user = {
+    email: 'test@example.com',
+    password: 'abc1',
+    login() {
+        // reach out to a database, check credentials, create a session
+    },
+    logout() {
+        // clear the session
+    }
+};
