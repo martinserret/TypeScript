3285bbefffdaa6d94f02fb77befb9a3dfe4b4dcc
Martin010 <martin.serret47@gmail.com>
2025-05-15 12:16:25 +0200

    Discriminated unions

--- 07. Advanced Types/code/02guard.js
+++ 07. Advanced Types/code/02guard.js
@@ -10,10 +10,28 @@ const dbSource = {
 function loadData(source) {
     // Open + read file OR reach out to database server
     if ('path' in source) {
-        console.log(source.path);
+        console.log(source.path); // Typescript understand that the source is FileSource
         // => use that to open the file locally
         return;
     }
-    console.log(source.connectionUrl);
+    console.log(source.connectionUrl); // Typescript understand that the source is DBSource
+    // => to reach out to database
+}
+const fileSourceDiscriminated = {
+    type: 'file',
+    path: 'some/path/to/file.csv',
+};
+const dbSourceDiscriminated = {
+    type: 'db',
+    connectionUrl: 'some-connection-url',
+};
+function loadDataDiscriminated(source) {
+    // Open + read file OR reach out to database server
+    if (source.type === 'file') {
+        console.log(source.path); // Typescript understand that the source is FileSourceDiscriminated
+        // => use that to open the file locally
+        return;
+    }
+    console.log(source.connectionUrl); // Typescript understand that the source is DBSourceDiscriminated
     // => to reach out to database
 }
--- 07. Advanced Types/code/02guard.ts
+++ 07. Advanced Types/code/02guard.ts
@@ -19,11 +19,45 @@ function loadData(source: Source) {
   // Open + read file OR reach out to database server
 
   if('path' in source) {
-    console.log(source.path);
+    console.log(source.path); // Typescript understand that the source is FileSource
     // => use that to open the file locally
     return;
   }
 
-  console.log(source.connectionUrl);
+  console.log(source.connectionUrl); // Typescript understand that the source is DBSource
+  // => to reach out to database
+}
+
+
+// DISCRIMINATED UNIONS
+// ----------------------------
+
+// The problem with checking the properties of a type is that the name of that properties might be changed in the futur.
+// Discriminated union pattern is to add a shared property which exists in both types (combined type) like "type" for example. Use a literal type feature to specific a string.
+
+type FileSourceDiscriminated = { type: 'file', path: string };
+const fileSourceDiscriminated: FileSourceDiscriminated = {
+  type: 'file',
+  path: 'some/path/to/file.csv',
+};
+
+type DBSourceDiscriminated = {  type: 'db', connectionUrl: string };
+const dbSourceDiscriminated: DBSourceDiscriminated = {
+  type: 'db',
+  connectionUrl: 'some-connection-url',
+};
+
+type SourceDiscriminated = FileSourceDiscriminated | DBSourceDiscriminated;
+
+function loadDataDiscriminated(source: SourceDiscriminated) {
+  // Open + read file OR reach out to database server
+
+  if(source.type === 'file') {
+    console.log(source.path); // Typescript understand that the source is FileSourceDiscriminated
+    // => use that to open the file locally
+    return;
+  }
+
+  console.log(source.connectionUrl); // Typescript understand that the source is DBSourceDiscriminated
   // => to reach out to database
 }
