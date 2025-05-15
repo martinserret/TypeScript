e603b29569579b156ce1482a02976c09397dcd4b
Martin010 <martin.serret47@gmail.com>
2025-05-15 16:02:11 +0200

    Function overloads

---
+++ 07. Advanced Types/code/03functions.js
@@ -0,0 +1,16 @@
+"use strict";
+// FUNCTION OVERLOADS
+// --------------------------
+// The problem: Typescript doesn't understand the specific type of a return value. Then, you can't use features of the different types.
+// For example: if Typescript thinks that the return value should be a "string" or a "number" you can't use length property of the type string even if you know that this value is a string
+function getLength(val) {
+    if (typeof val === 'string') {
+        const numberOfWords = val.split(' ').length;
+        return `${numberOfWords} words`; // return a string
+    }
+    return val.length; // return a number
+}
+// Typescript thinks that it's either a string or a number
+const numOfWords = getLength('Does this work?'); // Typescript don't understand that will be a string
+const numItems = getLength(['Sports', 'Cookies']); // Typescript don't understand that will be a number
+// numOfWords.length; //! error because Typescript thinks that value should be a number
