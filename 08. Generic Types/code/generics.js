789e4a5c1e9e24c29ca46a57ac2db8714d25aca7
Martin010 <martin.serret47@gmail.com>
2025-05-16 11:17:13 +0200

    understand generic types

--- 08. Generic Types/code/generics.js
+++ 08. Generic Types/code/generics.js
@@ -3,3 +3,9 @@
 // ---------------------------------------
 let namesArray = ['Dwight', 'Jim']; // Classis way to explicit the type: string[] = array of strings
 let names = ['Dwight', 'Jim']; // Array<string> is a generic type
+// UNDERSTAND GENERIC TYPES
+// ------------------------------------
+// generic types : types that need to work together with other types. So types where multiple types need to work together in order to accurately describe a certain value type.
+//  - Example : let names: Array<string> = ['Dwight', 'Jim']; => the type of this value is an array full of strings. We have 2 types working together : array and string
+//  - generic type is about combinations of types and building flexible things. Array is the generic type (always the thing in front of the angle brackets) and is pretty flexible
+//    because you can use this Array type to describe all kinds of arrays that contain all kinds of value types.
--- 08. Generic Types/code/generics.ts
+++ 08. Generic Types/code/generics.ts
@@ -5,3 +5,10 @@ let namesArray: string[] = ['Dwight', 'Jim'];  // Classis way to explicit the ty
 let names: Array<string> = ['Dwight', 'Jim']; // Array<string> is a generic type
 
 
+// UNDERSTAND GENERIC TYPES
+// ------------------------------------
+
+// generic types : types that need to work together with other types. So types where multiple types need to work together in order to accurately describe a certain value type.
+//  - Example : let names: Array<string> = ['Dwight', 'Jim']; => the type of this value is an array full of strings. We have 2 types working together : array and string
+//  - generic type is about combinations of types and building flexible things. Array is the generic type (always the thing in front of the angle brackets) and is pretty flexible
+//    because you can use this Array type to describe all kinds of arrays that contain all kinds of value types.
