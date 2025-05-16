a07ccd89935d94d0e562ee99e8458d3240fdbce0
Martin010 <martin.serret47@gmail.com>
2025-05-16 10:14:52 +0200

    Constant type with "as const"

--- 07. Advanced Types/code/04advanced.ts
+++ 07. Advanced Types/code/04advanced.ts
@@ -17,3 +17,17 @@ let store: DataStore = {};
 store.id = 5;
 store.isOpen = false;
 // store.name = 'Dwight' //! Error because I can add only properties with number or boolean values
+
+
+
+// CONSTANT TYPES WITH "AS CONST"
+// ---------------------
+
+// In certain situations, you might want Typescript to be a bit stricter and to not have it infer such a broad (large) type. 
+// Sometimes you might want to force Typescript ton infer a bit of a more narrow type.
+// You can do so by adding "as const" at the end. This is a Typescript feature 
+
+let roles = ["admin", "guest", "editor"] as const;
+// roles.push("visitor") //! Error because "roles" is readonly
+
+const firstRole = roles[0]; // Typescript knows that the role is "admin"
