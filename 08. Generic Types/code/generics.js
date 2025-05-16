82df64e12dafc599d43fc91353ed8c5ddf1a7f97
Martin010 <martin.serret47@gmail.com>
2025-05-16 12:51:21 +0200

    Generic function and inference

--- 08. Generic Types/code/generics.js
+++ 08. Generic Types/code/generics.js
@@ -2,9 +2,25 @@
 // A GENERIC TYPE WE ALREADY KNOW
 // ---------------------------------------
 let namesArray = ['Dwight', 'Jim']; // Classis way to explicit the type: string[] = array of strings
-let names = ['Dwight', 'Jim']; // Array<string> is a generic type
+let names = ['Dwight', 'Jim']; // Array<string> is a generic type (<string> is optional because Typescript infer (déduire) that the values in the array are strings)
 let store = {};
 store.name = 'Dwight';
 store.isInstructor = true;
 let nameStore = {};
 nameStore.id = 112;
+// GENERIC FUNCTIONS & INFERENCE
+// ------------------------------------
+// You're not limited to building generic types. You can also build generic functions.
+// The parameters could be "any" but the problem is that the return of teh function will be any too. The consequence is that Typescript doesn't have the information of type because this information is lost with "any".
+// Then, you can use autocompletion to use features link to a specific type.
+// To turn a function to a generic function is the same way as for a generic type ("<T>" with T as placeholder). Then place "T" as type for the parameters. 
+function mergeAny(a, b) {
+    return [a, b];
+}
+const idsAny = mergeAny(1, 2); // idsAny is "any[]" type => Typescript doesn't that the values are numbers so you can't use autocompletion
+function merge(a, b) {
+    return [a, b];
+}
+const ids = merge(1, 2); // ids type is "number[]". Typescript is able to infer (déduire) that "merge" return an array of numbers here because it takes a look at the value types of the 2 arguments passed
+ids[0].toExponential(2); // Now you have the autocompletion for number type
+const idsString = merge('Dwight', 'Jim'); // ids type is "string[]". Typescript infer that the arguments are strings
--- 08. Generic Types/code/generics.ts
+++ 08. Generic Types/code/generics.ts
@@ -2,7 +2,7 @@
 // ---------------------------------------
 
 let namesArray: string[] = ['Dwight', 'Jim'];  // Classis way to explicit the type: string[] = array of strings
-let names: Array<string> = ['Dwight', 'Jim']; // Array<string> is a generic type
+let names: Array<string> = ['Dwight', 'Jim']; // Array<string> is a generic type (<string> is optional because Typescript infer (déduire) that the values in the array are strings)
 
 
 // UNDERSTAND GENERIC TYPES
@@ -34,3 +34,31 @@ store.isInstructor = true;
 
 let nameStore: DataStore<number> = {};
 nameStore.id = 112; 
+
+
+
+// GENERIC FUNCTIONS & INFERENCE
+// ------------------------------------
+
+// You're not limited to building generic types. You can also build generic functions.
+
+// The parameters could be "any" but the problem is that the return of teh function will be any too. The consequence is that Typescript doesn't have the information of type because this information is lost with "any".
+// Then, you can use autocompletion to use features link to a specific type.
+
+// To turn a function to a generic function is the same way as for a generic type ("<T>" with T as placeholder). Then place "T" as type for the parameters. 
+
+function mergeAny(a: any, b: any) {
+  return [a, b];
+}
+
+const idsAny = mergeAny(1, 2); // idsAny is "any[]" type => Typescript doesn't that the values are numbers so you can't use autocompletion
+
+function merge<T>(a: T, b: T) {
+  return [a, b];
+}
+
+const ids = merge(1, 2); // ids type is "number[]". Typescript is able to infer (déduire) that "merge" return an array of numbers here because it takes a look at the value types of the 2 arguments passed
+ids[0].toExponential(2); // Now you have the autocompletion for number type
+
+const idsString = merge('Dwight', 'Jim'); // ids type is "string[]". Typescript infer that the arguments are strings
+
