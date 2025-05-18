f9786346ac032df471f81f702c2af2644b788670
Martin010 <martin.serret47@gmail.com>
2025-05-18 10:46:32 +0200

    Add a node method

--- 09. Classes & Generics - Demo Project/code/linked-list.ts
+++ 09. Classes & Generics - Demo Project/code/linked-list.ts
@@ -1,13 +1,25 @@
-class ListNode {
+class ListNode<T> {
+  next?: ListNode<T> // Optional because if this node is the last, there is not next node
 
+  constructor(public value: T) { }
 }
 
 class LinkedList<T> {
-  private root?: ListNode;
+  private root?: ListNode<T>;
   private length = 0;
 
   add(value: T) {
-
+    const node = new ListNode(value);
+    if (!this.root) {
+      this.root = node;
+    } else {
+      let current = this.root;
+      while (current.next) {
+        current = current.next;
+      }
+      current.next = node;
+    }
+    this.length++;
   }
 }
 
