e748481438ff1c1b666db1766d20f9fd14572bf0
Martin010 <martin.serret47@gmail.com>
2025-05-18 10:30:25 +0200

    Making LinkedList generic

--- 09. Classes & Generics - Demo Project/code/linked-list.ts
+++ 09. Classes & Generics - Demo Project/code/linked-list.ts
@@ -2,7 +2,14 @@ class ListNode {
 
 }
 
-class LinkedList {
+class LinkedList<T> {
   private root?: ListNode;
   private length = 0;
+
+  add(value: T) {
+
+  }
 }
+
+const numberList = new LinkedList<number>();
+const nameList = new LinkedList<string>();
