e8620d81a1cce011db29dc7a5b2323816823bb66
Martin010 <martin.serret47@gmail.com>
2025-05-19 15:11:54 +0200

    Read only and optional in mapping

--- 10. Deriving Types From Types/code/mapped-types.js
+++ 10. Deriving Types From Types/code/mapped-types.js
@@ -13,3 +13,19 @@ let mathResults = {
     add: mathOperations.add(1, 2),
     subtract: mathOperations.subtract(2, 1),
 };
+let mathResultsOptional = {
+    add: mathOperations.add(5, 10)
+};
+let mathResultsNonOptional = {
+    add: mathOperations.add(5, 10),
+    subtract: mathOperations.subtract(44, 8)
+};
+let mathResultsReadOnly = {
+    add: mathOperations.add(5, 10),
+    subtract: mathOperations.subtract(44, 8)
+};
+let mathResultsRemoveReadOnly = {
+    add: mathOperations.add(5, 10),
+    subtract: mathOperations.subtract(44, 8)
+};
+mathResultsRemoveReadOnly.add = 10; // No error because read only flag is removed
--- 10. Deriving Types From Types/code/mapped-types.ts
+++ 10. Deriving Types From Types/code/mapped-types.ts
@@ -34,3 +34,80 @@ let mathResults: Results<Operations> = { // This object can only use the Operati
   add: mathOperations.add(1, 2),
   subtract: mathOperations.subtract(2, 1),
 }
+
+
+
+// READONLY TYPES & OPTIONAL MAPPING
+// -------------------------------------------
+
+// mapped-types allow you to, for example, make properties optional event though if they're not optional in the original type you're mapping from
+// They allow you to make properties non-optional if they have been optional before
+
+// - properties non-optional become optional: add a question mark in the mapping => [key in keyof T]?: number
+//    You can also make your type properties optional with adding a question mark
+// - properties optional become non-optional: add a minus question mark "-?" Typescript feature in the mapping => [key in keyof T]-?: number
+
+// -?: removing the optional flag when working with mapped types
+
+// - You can make properties read only when working with mapped types: readonly [key in keyof T]?: number
+// - You can also remove the read only flag when working with mapped types: -readonly [key in keyof T]?: number
+
+// read only: property or method read only can't be overwritten once it has been defined
+
+
+// NON-OPTIONAL TO OPTIONAL
+type ResultsOptional<T> = {
+  [key in keyof T]?: number // Properties are now optional (because of the mapping)
+};
+
+let mathResultsOptional: ResultsOptional<Operations> = { // Allow to use only one property
+  add: mathOperations.add(5, 10)
+};
+
+
+
+// OPTIONAL TO NON-OPTIONAL
+type OperationsOptional = {
+  add?: (a: number, b: number) => number,
+  subtract?: (a: number, b: number) => number
+};
+
+type ResultsNonOptional<T> = {
+  [key in keyof T]-?: number // Properties are now non-optional (because of the mapping)
+};
+
+let mathResultsNonOptional: ResultsNonOptional<OperationsOptional> = { // All the properties must be used
+  add: mathOperations.add(5, 10),
+  subtract: mathOperations.subtract(44, 8)
+};
+
+
+// READ ONLY MAPPED-TYPE
+type ResultsReadOnly<T> = {
+  readonly [key in keyof T]?: number // Properties are now read only
+};
+
+let mathResultsReadOnly: ResultsReadOnly<Operations> = { // All the properties must be used
+  add: mathOperations.add(5, 10),
+  subtract: mathOperations.subtract(44, 8)
+};
+
+// mathResultsReadOnly.add = 10; //! Error because read only property
+
+
+// READ ONLY TYPE
+type OperationsReadOnly = {
+  readonly add: (a: number, b: number) => number,
+  readonly subtract: (a: number, b: number) => number
+};
+
+type ResultsRemoveReadOnly<T> = {
+  -readonly [key in keyof T]?: number // Properties are now read only
+};
+
+let mathResultsRemoveReadOnly: ResultsRemoveReadOnly<OperationsReadOnly> = { // All the properties must be used
+  add: mathOperations.add(5, 10),
+  subtract: mathOperations.subtract(44, 8)
+};
+
+mathResultsRemoveReadOnly.add = 10; // No error because read only flag is removed
