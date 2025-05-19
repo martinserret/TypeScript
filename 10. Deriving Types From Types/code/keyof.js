ce7eaf91981a6b6fadc1d45cc5832d31ab2bdfae
Martin010 <martin.serret47@gmail.com>
2025-05-19 09:58:51 +0200

    keyof useful example

--- 10. Deriving Types From Types/code/keyof.js
+++ 10. Deriving Types From Types/code/keyof.js
@@ -4,3 +4,18 @@
 let validKey; // "validKey" ne peut prendre que les valeurs des keys définies dans User
 validKey = 'name';
 validKey = 'age';
+// KEYOF USEFUL EXAMPLE
+// -------------------------
+// In this example, we create a function that check if a value stored in an object are not "undefined" or "null"
+// To retrieve the value of the object, we only want keys that exist in that object (first parameter) => keyof operator
+function getProp(obj, key) {
+    const val = obj[key];
+    if (val === undefined || val === null) {
+        throw new Error('Accessing undefined or null value.');
+    }
+    return val;
+}
+const user = { name: 'Dwight', age: 35 };
+const val = getProp(user, 'age'); // The second parameter can only be a string which is one of the two keys we have in "user"
+const data = { id: 1, isStored: false, values: [1, -15, 7] };
+const dataVal = getProp(data, 'isStored'); // The second parameter can only be a string which is one of the keys we have in "user"
--- 10. Deriving Types From Types/code/keyof.ts
+++ 10. Deriving Types From Types/code/keyof.ts
@@ -11,3 +11,27 @@ let validKey: UserKeys; // "validKey" ne peut prendre que les valeurs des keys d
 
 validKey = 'name';
 validKey = 'age';
+
+
+
+// KEYOF USEFUL EXAMPLE
+// -------------------------
+
+// In this example, we create a function that check if a value stored in an object are not "undefined" or "null"
+// To retrieve the value of the object, we only want keys that exist in that object (first parameter) => keyof operator
+
+function getProp<T extends object, U extends keyof T>(obj: T, key: U) {
+  const val = obj[key]
+
+  if (val === undefined || val === null) {
+    throw new Error('Accessing undefined or null value.')
+  }
+
+  return val;
+}
+
+const user = { name: 'Dwight', age: 35 };
+const val = getProp(user, 'age'); // The second parameter can only be a string which is one of the two keys we have in "user"
+
+const data = { id: 1, isStored: false, values: [1, -15, 7] }
+const dataVal = getProp(data, 'isStored'); // The second parameter can only be a string which is one of the keys we have in "user"
