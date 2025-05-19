51d8ddcaae19628ea3ae0028ead0dc9694cb94a7
Martin010 <martin.serret47@gmail.com>
2025-05-19 11:30:27 +0200

    Accessing array elements with indexed access types

--- 10. Deriving Types From Types/code/indexed-access-types.ts
+++ 10. Deriving Types From Types/code/indexed-access-types.ts
@@ -15,3 +15,17 @@ type AppUser = {
 };
 
 type Perms = AppUser['permissions']
+
+
+
+// ACCESSING ARRAY ELEMENTS WITH INDEXED ACCESS TYPES
+// ----------------------------------------------------------
+
+// You can also use it on arrays to extract the value type of the elements that are stored in the array
+// You must use [number] to tell Typescript that you want to extract the type of the elements that are stored for the different INDEXES in the array
+
+type Perm = Perms[number]; // Tells Typescript that you want to extract the type of the elements that are stored for the different indexes in the array
+
+
+type Names = string[];
+type Name = Names[number]; // Let Typescript look into the array type and take a look at the type of the values that will be stored in that array and then give you just that value type
