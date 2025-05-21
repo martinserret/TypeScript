7c4e32eb9f13bce08f3850cdb1676dd4c97e0734
Martin010 <martin.serret47@gmail.com>
2025-05-21 12:17:52 +0200

    WithTemplate decorator factory

--- 12. Experimental Decorators/code/src/app.ts
+++ 12. Experimental Decorators/code/src/app.ts
@@ -19,6 +19,13 @@
 //  - a decorator factory accept arguments and pass it to decorator functions that it execute
 //  - then we you call the decorator factory, you can pass customize values which will be used by inner returned decorator function
 
+
+// BUILDING MORE USEFUL DECORATORS (WithTemplate)
+// ------------------------------------
+
+// if you don't use an argument in a function, you can use the symbol "_" to tell Typescript : "I know I get this argument but I don't need it"
+// WithTemplate decorator: allow to add html element in your html file by passing an html id (here h1 element where we add the name of the Person object)
+
 function Logger(logString: string) {
   return function (target: Function) {
     console.log(logString)
@@ -26,7 +33,20 @@ function Logger(logString: string) {
   };
 }
 
-@Logger('LOGGING - PERSON')
+function WithTemplate(template: string, hookId: string) {
+  return function (constructor: any) {
+    const hookEl = document.getElementById(hookId);
+    const p = new constructor(); // here constructor is the class Person
+    if (hookEl) {
+      hookEl.innerHTML = template
+      hookEl.querySelector('h1')!.textContent += p.name
+    }
+  }
+}
+
+
+// @Logger('LOGGING - PERSON')
+@WithTemplate("<h1>My person object </h1>", "app")
 class Person {
   name = "Dwight"
 
