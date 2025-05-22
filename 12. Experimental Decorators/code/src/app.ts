e09cdff6f89a2d07b5b3304e205e3bc73c32da49
Martin010 <martin.serret47@gmail.com>
2025-05-22 15:59:31 +0200

    Validation with decorators next (experimental)

--- 12. Experimental Decorators/code/src/app.ts
+++ 12. Experimental Decorators/code/src/app.ts
@@ -304,16 +304,50 @@ buttonBind.addEventListener('click', pb.showMessage);
 
 // It could be nice to have a validation logic decorators that valid or invalid data depends on type,length, empty, etc. that we can use for all validations
 
-function Required() {
-
+interface ValidatorConfig {
+  [property: string]: {
+    [validatableProp: string]: string[] // ['required', 'positive', ...]
+  }
 }
 
-function PositiveNumber() {
+const registeredValidators: ValidatorConfig = {};
 
+function Required(target: any, propName: string) {
+  registeredValidators[target.constructor.name] = {
+    ...registeredValidators[target.constructor.name],
+    [propName]: ['required']
+  };
 }
 
-function validate(obj: object) {
+function PositiveNumber(target: any, propName: string) {
+  registeredValidators[target.constructor.name] = {
+    ...registeredValidators[target.constructor.name],
+    [propName]: ['positive']
+  };
+}
 
+function validate(obj: any) {
+  const objValidatorConfig = registeredValidators[obj.constructor.name];
+  if (!objValidatorConfig) {
+    return true;
+  }
+
+  let isValid = true;
+
+  for (const prop in objValidatorConfig) {
+    for (const validator of objValidatorConfig[prop]) {
+      switch (validator) {
+        case 'required':
+          isValid = isValid && !!obj[prop]; //return true or false if an obj[prop] exists
+          break;
+        case 'positive':
+          isValid = isValid && obj[prop] > 0;
+          break;
+      }
+    }
+  }
+
+  return isValid;
 }
 
 class Course {
