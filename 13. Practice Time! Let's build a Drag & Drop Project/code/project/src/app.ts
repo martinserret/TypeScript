86dc81772a6ca64b647f55a2c1303887484f9be9
Martin010 <martin.serret47@gmail.com>
2025-05-23 11:58:33 +0200

    Interacting with input elements : - get the input elements - add an event listener to the submit button - don't forget bind (context = current instance)

--- 13. Practice Time! Let's build a Drag & Drop Project/code/project/src/app.ts
+++ 13. Practice Time! Let's build a Drag & Drop Project/code/project/src/app.ts
@@ -3,17 +3,44 @@ class ProjectInput {
   hostElement: HTMLDivElement;
   element: HTMLFormElement;
 
+  titleInputElement: HTMLInputElement;
+  descriptionInputElement: HTMLInputElement;
+  peopleInputElement: HTMLInputElement;
+
   constructor() {
+    // GET THE TEMPLATE AND HOST ELEMENTS
     this.templateElement = document.getElementById('project-input')! as HTMLTemplateElement;
     this.hostElement = document.getElementById('app')! as HTMLDivElement;
 
+    // CREATE THE ELEMENT
     const importedNode = document.importNode(this.templateElement.content, true); // importNode(template, deep) creates a copy of the template content
     this.element = importedNode.firstElementChild as HTMLFormElement; // firstElementChild returns the first child element of the specified element
+    this.element.id = 'user-input';
+
+    // GET THE INPUT ELEMENTS
+    this.titleInputElement = this.element.querySelector('#title')! as HTMLInputElement;
+    this.descriptionInputElement = this.element.querySelector('#description')! as HTMLInputElement;
+    this.peopleInputElement = this.element.querySelector('#people')! as HTMLInputElement;
+
+    this.configure();
+
     this.attach();
   }
 
+  private submitHandler(event: Event) {
+    event.preventDefault();
+    console.log(this.titleInputElement.value);
+    console.log(this.descriptionInputElement.value);
+    console.log(this.peopleInputElement.value);
+
+  }
+
+  private configure() {
+    this.element.addEventListener('submit', this.submitHandler.bind(this)); // bind(this) binds the context of the function to the current instance of the class. So that 'this' inside the function refers to the class instance
+  }
+
   private attach() {
-    this.hostElement.insertAdjacentElement('afterbegin', this.element) // insertAdjacentElement(where, element) inserts the element at the specified position relative to the target element
+    this.hostElement.insertAdjacentElement('afterbegin', this.element); // insertAdjacentElement(where, element) inserts the element at the specified position relative to the target element
   }
 }
 
