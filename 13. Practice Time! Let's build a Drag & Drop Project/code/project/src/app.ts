ca07de7992b65f184a283c773806b94defdd13b2
Martin010 <martin.serret47@gmail.com>
2025-05-26 09:44:40 +0200

    End inheritance : - Adaptation of the class ProjectList - New class State - ProjectState extends State

--- 13. Practice Time! Let's build a Drag & Drop Project/code/project/src/app.ts
+++ 13. Practice Time! Let's build a Drag & Drop Project/code/project/src/app.ts
@@ -6,15 +6,23 @@ class Project { // here a class and not an interface or type is used because we 
 }
 
 // PROJECT STATE MANAGEMENT
-type Listener = (items: Project[]) => void; // This type defines a function that takes an array of Project objects as an argument and returns void. It is used to define the type of the listener functions that will be added to the state management.
+type Listener<T> = (items: T[]) => void; // This type defines a function that takes an array of Project objects as an argument and returns void. It is used to define the type of the listener functions that will be added to the state management.
 
-class ProjectState {
-  private listeners: Listener[] = [];
+class State<T> {
+  protected listeners: Listener<T>[] = [];
+
+  addListener(listenerFunction: Listener<T>) {
+    this.listeners.push(listenerFunction);
+  }
+}
+
+class ProjectState extends State<Project> {
   private projects: Project[] = [];
   private static instance: ProjectState;
 
   private constructor() {
     // This is a singleton class, so the constructor is private to prevent instantiation from outside the class.
+    super();
   }
 
   static getInstance() { // This method returns the singleton instance of the ProjectState class.
@@ -33,10 +41,6 @@ class ProjectState {
     for (const listener of this.listeners) {
       listener(this.projects.slice()); // slice() creates a shallow copy of the array, so that the original array is not modified
     }
-  }
-
-  addListener(listenerFunction: Listener) {
-    this.listeners.push(listenerFunction);
   }
 }
 
@@ -135,6 +139,11 @@ class ProjectList extends Component<HTMLDivElement, HTMLElement> {
     super('project-list', 'app', false, `${type}-projects`);
     this.assignedProjects = []; // Initialize the assignedProjects array
 
+    this.configure(); // Call the configure method to set up the listener for project updates
+    this.renderContent(); // Call the renderContent method to set up the initial content of the project list
+  }
+
+  configure() {
     projectState.addListener((projects: Project[]) => {
       const relevantProjects = projects.filter(project => {
         if (this.type === 'active') {
@@ -146,8 +155,12 @@ class ProjectList extends Component<HTMLDivElement, HTMLElement> {
       this.assignedProjects = relevantProjects;
       this.renderProjects();
     })
+  }
 
-    this.renderContent();
+  renderContent() {
+    const listId = `${this.type}-projects-list`;
+    this.element.querySelector("ul")!.id = listId; // querySelector(selector) returns the first element that matches the specified selector. The "!" asserts that the value is not null or undefined.
+    this.element.querySelector("h2")!.textContent = this.type.toUpperCase() + " PROJECTS";
   }
 
   private renderProjects() {
@@ -159,37 +172,18 @@ class ProjectList extends Component<HTMLDivElement, HTMLElement> {
       listElement.appendChild(listItem);
     }
   }
-
-  configure() { }
-
-  renderContent() {
-    const listId = `${this.type}-projects-list`;
-    this.element.querySelector("ul")!.id = listId; // querySelector(selector) returns the first element that matches the specified selector. The "!" asserts that the value is not null or undefined.
-    this.element.querySelector("h2")!.textContent = this.type.toUpperCase() + " PROJECTS";
-  }
 }
 
 
 
 // PROJECTINPUT CLASS
-class ProjectInput {
-  templateElement: HTMLTemplateElement;
-  hostElement: HTMLDivElement;
-  element: HTMLFormElement;
-
+class ProjectInput extends Component<HTMLDivElement, HTMLFormElement> {
   titleInputElement: HTMLInputElement;
   descriptionInputElement: HTMLInputElement;
   peopleInputElement: HTMLInputElement;
 
   constructor() {
-    // GET THE TEMPLATE AND HOST ELEMENTS
-    this.templateElement = document.getElementById('project-input')! as HTMLTemplateElement;
-    this.hostElement = document.getElementById('app')! as HTMLDivElement;
-
-    // CREATE THE ELEMENT
-    const importedNode = document.importNode(this.templateElement.content, true); // importNode(template, deep) creates a copy of the template content
-    this.element = importedNode.firstElementChild as HTMLFormElement; // firstElementChild returns the first child element of the specified element
-    this.element.id = 'user-input';
+    super('project-input', 'app', true, 'user-input');
 
     // GET THE INPUT ELEMENTS
     this.titleInputElement = this.element.querySelector('#title')! as HTMLInputElement;
@@ -197,9 +191,13 @@ class ProjectInput {
     this.peopleInputElement = this.element.querySelector('#people')! as HTMLInputElement;
 
     this.configure();
-
-    this.attach();
   }
+
+  configure() {
+    this.element.addEventListener('submit', this.submitHandler); // bind(this) binds the context of the function to the current instance of the class. So that 'this' inside the function refers to the class instance
+  }
+
+  renderContent() { }
 
   private gatherUserInputs(): [string, string, number] | void { // [string, string, number] is the way to define a tuple type in TypeScript. Here, exactly three values are expected: a string, a string, and a number respectively.
     const enteredTitle = this.titleInputElement.value;
@@ -252,14 +250,6 @@ class ProjectInput {
       projectState.addProject(title, description, people); // Add the project to the state management
       this.clearInputs();
     }
-  }
-
-  private configure() {
-    this.element.addEventListener('submit', this.submitHandler); // bind(this) binds the context of the function to the current instance of the class. So that 'this' inside the function refers to the class instance
-  }
-
-  private attach() {
-    this.hostElement.insertAdjacentElement('afterbegin', this.element); // insertAdjacentElement(where, element) inserts the element at the specified position relative to the target element
   }
 }
 
