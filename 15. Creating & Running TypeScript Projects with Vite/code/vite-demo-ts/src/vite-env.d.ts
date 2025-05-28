e588005ddff368487c4771c1a76e4a04bc07af38
Martin010 <martin.serret47@gmail.com>
2025-05-28 15:20:57 +0200

    Making sense of .d.ts files

--- 15. Creating & Running TypeScript Projects with Vite/code/vite-demo-ts/src/vite-env.d.ts
+++ 15. Creating & Running TypeScript Projects with Vite/code/vite-demo-ts/src/vite-env.d.ts
@@ -1,1 +1,8 @@
-/// <reference types="vite/client" />
+// .d.ts files are TypeScript declaration files (or type declaration files). They don't specify code that should be executed. 
+// They contain just type definitions that can be used by other files, but that can't be executed (doesn't compile to JavaScript).
+
+
+// Here, points at some other file that is part of the vite package which is stored in the node_modules folder.
+// Contains type definitions for Vite's environment variables and other global types that Vite provides.
+/// <reference types="vite/client" /> 
+
