9378fbc55c7579a213b6e17efe3b37d59e515454
Martin010 <martin.serret47@gmail.com>
2025-05-29 11:44:49 +0200

    Add devServer in webpack.config.js

--- 16. Using Webpack with TypeScript/code/project-webpack-compilation/webpack.config.js
+++ 16. Using Webpack with TypeScript/code/project-webpack-compilation/webpack.config.js
@@ -5,8 +5,16 @@ module.exports = {
   output: {
     filename: 'bundle.js',
     path: path.resolve(__dirname, 'dist'), // Absolute path to the output directory
+    publicPath: '/dist/' // Public URL of the output directory when referenced in the browser
   },
   devtool : 'inline-source-map', // Enable source maps for easier debugging
+  devServer: {
+    static: [
+      {
+        directory: path.join(__dirname),
+      },
+    ],
+  },
   module: {
     rules: [
       {
@@ -18,9 +26,10 @@ module.exports = {
   },
   resolve: {
     extensions: ['.ts', '.js'], // Resolve these extensions
-  }
+  },
 };
 
 // module: in the end is just a file. We tell Webpack how to work with different file types it finds (ts, js, html, css, etc.). In our case: only TypeScript files.
 // resolve: which file extensions Webpack should resolve. By default, it only resolves .js files, so we add .ts to it.
 // devtool: tells webpack that there will be generated source maps. This is useful for debugging, as it allows you to see the original TypeScript code in the browser's developer tools instead of the compiled JavaScript code.
+// devServer: configuration for the development server. It serves files from the current directory (__dirname) and allows you to access your application in the browser during development.
