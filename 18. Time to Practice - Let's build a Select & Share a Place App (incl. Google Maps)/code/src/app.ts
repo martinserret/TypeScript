97c422e22322220a114249707cf44a1e7553babf
Martin010 <martin.serret47@gmail.com>
2025-05-30 16:20:39 +0200

    Get user input

--- 18. Time to Practice - Let's build a Select & Share a Place App (incl. Google Maps)/code/src/app.ts
+++ 18. Time to Practice - Let's build a Select & Share a Place App (incl. Google Maps)/code/src/app.ts
@@ -1,1 +1,12 @@
-// Code goes here!
+const form = document.querySelector<HTMLFormElement>('form')!;
+const addressInput = document.getElementById('address')! as HTMLInputElement;
+
+function searchAddressHandler(event: Event) {
+  event.preventDefault();
+  const enteredAddress = addressInput.value;
+
+  // Send to Google Maps API
+
+}
+
+form.addEvenListener('submit', searchAddressHandler);
