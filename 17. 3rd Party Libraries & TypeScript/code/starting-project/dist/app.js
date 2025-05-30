1d4461ebe5e55bf119c933a9f85d857e398bc2f1
Martin010 <martin.serret47@gmail.com>
2025-05-30 11:57:35 +0200

    Zod Typescript advantage during runtime

--- 17. 3rd Party Libraries & TypeScript/code/starting-project/dist/app.js
+++ 17. 3rd Party Libraries & TypeScript/code/starting-project/dist/app.js
@@ -8,7 +8,15 @@ const chunkedArray = _.chunk(numbers, 2);
 // --------------------
 import fs from 'node:fs';
 import { z } from 'zod';
-const content = fs.readFileSync('data.json');
-const dataSchema = z.number();
-const parsedData = dataSchema.parse(content); // This will throw an error if content is not a string during the runtime
+const stringSchema = z.string();
+const parsedString = stringSchema.parse('Hello World'); // If there is no error, parsedString will be a string without defining a type explicitly. Huge thing !
+const content = JSON.parse(fs.readFileSync('data.json').toString());
+// const dataSchemaNumber = z.number();
+// const parsedDataNumber = dataSchemaNumber.parse(content) //! This will throw an error if content is not a string during the runtime
+const dataSchema = z.object({
+    title: z.string(),
+    id: z.number(),
+    value: z.array(z.union([z.string(), z.number()]))
+});
+const parsedData = dataSchema.parse(content); // If there is no error, parsedData will be an object without defining a type explicitly. Huge thing !
 //# sourceMappingURL=app.js.map
