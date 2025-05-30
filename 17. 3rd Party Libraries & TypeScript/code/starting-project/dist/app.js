35590112fd0a3d655e89b5b0c51eaacba0f17d3b
Martin010 <martin.serret47@gmail.com>
2025-05-30 11:21:21 +0200

    The Typescript library Zod

--- 17. 3rd Party Libraries & TypeScript/code/starting-project/dist/app.js
+++ 17. 3rd Party Libraries & TypeScript/code/starting-project/dist/app.js
@@ -1,21 +1,14 @@
 // LODASH EXAMPLE
 // --------------------
-
 import _ from 'lodash';
-
 const numbers = [1, 2, 3, 4, 5];
-
 // split that into multiple arrays
 const chunkedArray = _.chunk(numbers, 2);
-
-
 // ZOD EXAMPLE
 // --------------------
 import fs from 'node:fs';
-
 import { z } from 'zod';
-
 const content = fs.readFileSync('data.json');
-
 const dataSchema = z.number();
-const parsedData = dataSchema.parse(content) // This will throw an error if content is not a string during the runtime
+const parsedData = dataSchema.parse(content); // This will throw an error if content is not a string during the runtime
+//# sourceMappingURL=app.js.map
