9a663cfd433c202d16e05a668831483ef29d7052
Martin010 <martin.serret47@gmail.com>
2025-06-02 09:18:58 +0200

    Managing state with useState

--- 19. React.js & TypeScript/code/react-ts/src/App.tsx
+++ 19. React.js & TypeScript/code/react-ts/src/App.tsx
@@ -1,9 +1,24 @@
+import { useState } from "react";
+
 import Header from "./components/Header";
 import CourseGoals from "./components/CourseGoals";
 
 import goalsImage from "./assets/goals.jpg";
 
 function App() {
+  const [goals, setGoals] = useState([
+    {
+      id:1, 
+      title: "Learn TS", 
+      description: "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Iusto consectetur minima similique. Voluptatem nam nisi ex possimus culpa suscipit eligendi!"
+    },
+    {
+      id:2, 
+      title: "Learn Japanese", 
+      description: "Lorem ipsum dolor sit, amet consectetur adipisicing elit."
+    }
+  ]);
+
 
   return (
     <main>
@@ -11,18 +26,7 @@ function App() {
         <h1>My Course Goals</h1>
       </Header>
       <CourseGoals 
-        goals={[
-          {
-            id:1, 
-            title: "Learn TS", 
-            description: "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Iusto consectetur minima similique. Voluptatem nam nisi ex possimus culpa suscipit eligendi!"
-          },
-          {
-            id:2, 
-            title: "Learn Japanese", 
-            description: "Lorem ipsum dolor sit, amet consectetur adipisicing elit."
-          }
-        ]}
+        goals={goals}
       />
     </main>
   );
