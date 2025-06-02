817c69a11cb95dfd93b2a39bfb263e43c482a67f
Martin010 <martin.serret47@gmail.com>
2025-06-02 11:06:18 +0200

    React, Typescript and refs: - Add 2 refs to the inputs - add explicit types to useRef (not take the type of the initial value) - store the inputs values in const

--- 19. React.js & TypeScript/code/react-ts/src/components/NewGoal.tsx
+++ 19. React.js & TypeScript/code/react-ts/src/components/NewGoal.tsx
@@ -1,9 +1,18 @@
-import { type FormEvent } from 'react';
+import { useRef, type FormEvent } from 'react';
 
 export default function NewGoal() {
+  // here, we use ref to access the input elements directly but we could also use controlled components with useState
+  const goalRef = useRef<HTMLInputElement>(null); // We add an explicit type to the ref to indicate that it will hold an HTMLInputElement and not the type of the initial value (which is null).
+  const summaryRef = useRef<HTMLInputElement>(null);// We add an explicit type to the ref to indicate that it will hold an HTMLInputElement and not the type of the initial value (which is null).
+  
   // In this case, the event type is FormEvent, which is a generic type that can be used for form events in React.
   function handleSubmit(event: FormEvent<HTMLFormElement>) {
     event.preventDefault();
+
+    const enteredGoal = goalRef.current!.value; // "!" is a non-null assertion operator, which tells TypeScript that we are sure goalRef.current is not null.
+    const enteredSummary = summaryRef.current!.value; // "!" is a non-null assertion operator, which tells TypeScript that we are sure goalRef.current is not null.
+
+    // validation...
 
     
   }
@@ -12,11 +21,11 @@ export default function NewGoal() {
     <form onSubmit={handleSubmit}>
       <p>
         <label htmlFor="goal">Your Goal</label>
-        <input type="text" id="goal" />
+        <input type="text" id="goal" ref={goalRef} />
       </p>
       <p>
         <label htmlFor="summary">Short Summary</label>
-        <input type="text" id="summary" />
+        <input type="text" id="summary" ref={summaryRef} />
       </p>
       <p>
         <button>Add Goal</button>
