acf1775c34dd53cde379497dd998f82d5287a85c
Martin010 <martin.serret47@gmail.com>
2025-06-02 10:34:31 +0200

    Handling form submission: - NewGoal.tsx - form creation - handleSubmit function with event type

--- 19. React.js & TypeScript/code/react-ts/src/components/NewGoal.tsx
+++ 19. React.js & TypeScript/code/react-ts/src/components/NewGoal.tsx
@@ -1,5 +1,26 @@
+import { type FormEvent } from 'react';
+
 export default function NewGoal() {
+  // In this case, the event type is FormEvent, which is a generic type that can be used for form events in React.
+  function handleSubmit(event: FormEvent<HTMLFormElement>) {
+    event.preventDefault();
+
+    
+  }
+  
   return (
-    <div>NewGoal</div>
-  )
+    <form onSubmit={handleSubmit}>
+      <p>
+        <label htmlFor="goal">Your Goal</label>
+        <input type="text" id="goal" />
+      </p>
+      <p>
+        <label htmlFor="summary">Short Summary</label>
+        <input type="text" id="summary" />
+      </p>
+      <p>
+        <button>Add Goal</button>
+      </p>
+    </form>
+  );
 }
