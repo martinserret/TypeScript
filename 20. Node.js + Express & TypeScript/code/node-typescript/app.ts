c91bdb2685d64079dd32a2e3c554fe4508e6c90b
Martin010 <martin.serret47@gmail.com>
2025-06-02 17:36:15 +0200

    Create a server: - install @types/node package to use Node.js types in Typescript - creation and test of the server (port 3000, display in the console the request and return an Hello World

--- 20. Node.js + Express & TypeScript/code/node-typescript/app.ts
+++ 20. Node.js + Express & TypeScript/code/node-typescript/app.ts
@@ -1,1 +1,8 @@
-console.log('Hello World!');
+import { createServer } from 'node:http'; // You have to install the @types/node package to use Node.js types in TypeScript (npm install @types/node)
+
+const server = createServer((req, res) => {
+  console.log(`Request received: ${req.method} ${req.url}`);
+  res.end('Hello World!');
+})
+
+server.listen(3000);
--- 20. Node.js + Express & TypeScript/code/node-typescript/dist/app.js
+++ 20. Node.js + Express & TypeScript/code/node-typescript/dist/app.js
@@ -1,2 +1,6 @@
-console.log('Hello World!');
-export {};
+import { createServer } from 'node:http'; // You have to install the @types/node package to use Node.js types in TypeScript (npm install @types/node)
+const server = createServer((req, res) => {
+    console.log(`Request received: ${req.method} ${req.url}`);
+    res.end('Hello World!');
+});
+server.listen(3000);
