7f5a01702ed5bab07d8eb60062ef34d559c96d17
Martin010 <martin.serret47@gmail.com>
2025-06-02 18:07:58 +0200

    Using Express to create a server: - install Express library - install @types/express to use Express types in TypeScript - creation and test of the server

--- 20. Node.js + Express & TypeScript/code/node-typescript/app.ts
+++ 20. Node.js + Express & TypeScript/code/node-typescript/app.ts
@@ -1,8 +1,26 @@
-import { createServer } from 'node:http'; // You have to install the @types/node package to use Node.js types in TypeScript (npm install @types/node)
+// CREATE A SERVER USING NODE.JS
+// ----------------------------------
 
-const server = createServer((req, res) => {
+// import { createServer } from 'node:http'; // You have to install the @types/node package to use Node.js types in TypeScript (npm install @types/node)
+
+// const server = createServer((req, res) => {
+//   console.log(`Request received: ${req.method} ${req.url}`);
+//   res.end('Hello World!');
+// })
+
+// server.listen(3000);
+
+
+
+// CREATE A SERVER USING EXPRESS
+// ----------------------------------
+import express from 'express'; // Express is not a library that has Typescript support built in. You have to install the @types/express package to use Express types in TypeScript (npm install --save-dev @types/express)
+
+const app = express();
+
+app.get('/', (req, res) => {
   console.log(`Request received: ${req.method} ${req.url}`);
-  res.end('Hello World!');
+  res.json({ message: 'Hello World!' });
 })
 
-server.listen(3000);
+app.listen(3000);
