4ba0cb5aa588f2e78782f481b70d24f809f3bcd9
Martin010 <martin.serret47@gmail.com>
2025-06-03 14:47:20 +0200

    Native Node.js Typescript and enum type

--- 20. Node.js + Express & TypeScript/code/node-typescript/routes/todo.ts
+++ 20. Node.js + Express & TypeScript/code/node-typescript/routes/todo.ts
@@ -2,11 +2,15 @@ import express, { type Request, type Response } from 'express';
 
 import { addTodo, getTodo, getTodos, removeTodo, updateTodo } from '../data.ts'; // Import the addTodo function from data.js and not .ts (if you use native Node.js Typescript support, use .ts)
 
+//! enum can't be used with native Node.js Typescript types. Because, unlike interfaces and types aliases, enum can't be removed to turn this code into JavaScript. This is a Typescript feature that has a representation in JavaScript but a compilation step is needed in order to have that representation in JavaScript. It can work if you use "--experimental-transform-types" flag when running the code with Node.js. The best solution is to not use the enum feature at all and use a string literal type instead.
+// enum TODO_TYPE {
+//   BASIC,
+//   URGENT
+// }
+
 const router = express.Router();
 
-
 // function handlePostTodos(req: Request, res: Response) {} // Here you have to use the Request and Response types from express to type the req and res parameters
-
 
 router.post('/todos', (req, res) => { // In that case, Typescript will infer the types of req and res parameters as Request and Response respectively
   const text = req.body.text; // no error because req.body is typed as any because we could be getting any kind of request body. So be careful with that.
